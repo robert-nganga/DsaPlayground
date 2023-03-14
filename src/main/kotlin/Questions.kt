d9f6c6cb3c97dd914564c27d5b4bfbf24ac7fcdf
robert-nganga <robertnganga220@gmail.com>
2023-03-14 13:28:02 +0300

    more questions

--- src/main/kotlin/Questions.kt
+++ src/main/kotlin/Questions.kt
@@ -1,22 +1,155 @@
-fun main(){
-    val arr = intArrayOf(-1,-1,0,0,-1,-1)
-    val map = hashMapOf<Int, String>(1 to "a", 2 to "b")
-    println(isIsomorphic("not", "bet"))
+import java.util.Stack
 
-    println(pivotIndex(arr))
+fun main(){
+//    val arr = intArrayOf(-1,-1,0,0,-1,-1)
+//    val map = hashMapOf<Int, String>(1 to "a", 2 to "b")
+//    println(isIsomorphic("not", "bet"))
+//    println(pivotIndex(arr))
+//
+//    val list = ListNode(`val` = 6, next = ListNode(`val` = 2, next = ListNode(`val` = 7, next = null)))
+//    println(reverseList(list))
+
+    println()
+
+
+}
+
+/*
+Question 7
+Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
+
+Nary-Tree input serialization is represented in their level order traversal.
+Each group of children is separated by the null value (See examples)
+ */
+class Node(var `val`: Int) {
+    var children: List<Node?> = listOf()
+}
+fun preorderStack(root: Node?): List<Int>{
+    val result = mutableListOf<Int>()
+    if (root == null) return result
+    val stack = Stack<Node>()
+    stack.push(root)
+    while (stack.isNotEmpty()){
+        val current = stack.pop()
+        result.add(current.`val`)
+        if (current.children.isNotEmpty()){
+            val size = current.children.size -1
+            for (i in size downTo 0 ){
+                stack.push(current.children[i])
+            }
+        }
+    }
+    return result
+}
+fun preorder(root: Node?): List<Int> {
+    val result = mutableListOf<Int>()
+    if (root == null) return result
+    result.add(root.`val`)
+    var head = root
+    head.children.forEach { child->
+        if (child!= null){
+            result.add(child.`val`)
+            preOrderRecurse(child.children, result)
+        }
+    }
+
+    return result
+}
+
+fun preOrderRecurse(nodes:List<Node?>, list: MutableList<Int>):List<Int>{
+    if (nodes.isEmpty()) return list
+    nodes.forEach {child->
+        if (child!= null){
+            list.add(child.`val`)
+            preOrderRecurse(child.children, list)
+        }
+    }
+    return list
+}
+
+/*
+Question6
+Given a string s which consists of lowercase or uppercase letters,
+return the length of the longest palindrome that can be built with those letters.
+
+Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+ */
+fun longestPalindrome(s: String): Int {
+    if (s.length == 1) return 1
+    val map = hashMapOf<Char, Int>()
+    for (str in s){
+        if (map.contains(str)){
+            map[str] = map[str]!! + 1
+        }else{
+            map[str] = 1
+        }
+    }
+    var oddTimes = 0
+    var evenTimes = 0
+    var length = 0
+    for ((_, num) in map){
+        val remainder = num % 2
+        if (remainder == 1){
+            length += num-1
+            oddTimes++
+        }
+        if (remainder == 0){
+            length += num
+            evenTimes++
+        }
+    }
+    return if (oddTimes >= 1) length+1 else length
+}
+
+/*
+Question 5
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+ */
+fun middleNode(head: ListNode?): ListNode? {
+    var map = arrayOf<ListNode?>()
+    var current = head
+    while (current != null){
+        map = map.plus(current)
+        current = current.next
+    }
+    val middle = map.size/2
+    return map[middle]
+
 }
 
 /*
 Question 4
 Given the head of a singly linked list, reverse the list, and return the reversed list.
  */
-fun reverseList(head: ListNode?) {
+
+fun reverseListRecursively(head: ListNode?): ListNode?{
+
+    if (head?.next == null) {
+        return head
+    }
+    val newHead = reverseList(head.next)
+    head.next?.next = head
+    head.next = null
+    return newHead
+
+}
+
+fun reverseList(head: ListNode?): ListNode? {
     var current = head
     var first = head
     var second = first?.next
 
-    while (second != null){}
-
+    while (second != null){
+        val temp = second.next
+        second.next = first
+        first = second
+        second = temp
+    }
+    current?.next = null
+    current = first
+    return current
 }
 
 /*
